f40ecb224c271ecbd3bed19921ec309ea47722ed
Nathan Gebremichael <nathan.gebremichael@mediendesign.de>
2023-10-26 12:55:48 +0200

    changed to mistral leo german

--- constants.py
+++ constants.py
@@ -153,11 +153,11 @@ MODEL_BASENAME = "model.safetensors"
 # MODEL_ID = "TheBloke/Nous-Hermes-13B-GPTQ"
 # MODEL_BASENAME = "nous-hermes-13b-GPTQ-4bit-128g.no-act.order"
 # MODEL_ID = "TheBloke/WizardLM-13B-V1.2-GPTQ"
-# MODEL_BASENAME = "gptq_model-4bit-128g.safetensors
+# MODEL_BASENAME = "gptq_model-4bit-128g.safetensors"
 # MODEL_ID = "TheBloke/em_german_13b_v01-GPTQ"
 # MODEL_ID = "TheBloke/Vicuna-13B-v1.3-German-GPTQ"
-MODEL_ID = "jphme/em_german_7b_v01_gptq"
-# MODEL_ID = "jphme/em_german_7b_leo_gptq"
+# MODEL_ID = "jphme/em_german_7b_v01_gptq"
+MODEL_ID = "jphme/em_german_7b_leo_gptq"
 
 ### 30b GPTQ Models for 24GB GPUs (*** Requires using intfloat/e5-base-v2 instead of hkunlp/instructor-large as embedding model ***)
 # MODEL_ID = "TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ"
